adb618b5414cef2c45b37826a6b9b091bd722faa
otto-dae <otto.acha.28@gmail.com>
2025-03-03 19:42:47 -0600

    pluh

--- src/app/dwlrecord/layout.tsx
+++ src/app/dwlrecord/layout.tsx
@@ -1,12 +1,14 @@
+'use client'
+
 import React from "react";
 
 export default function(){
     return (
-    <>
+    
     <html>
         <body>
-            
+
         </body>
     </html>
-    </>)
+    )
 }
