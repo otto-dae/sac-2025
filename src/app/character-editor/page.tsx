a3f75a44c67c8a7e9f8db30f056740a522abbaed
Juan Antonio HR <92959694+JuanAntonioHR@users.noreply.github.com>
2025-03-09 23:02:39 -0600

    add: implement utility functions and components for color management and theming

--- src/app/character-editor/page.tsx
+++ src/app/character-editor/page.tsx
@@ -2,6 +2,9 @@
 
 import Image, { StaticImageData } from 'next/image'
 import React, { useState } from 'react';
+import { parseColor } from "@react-stately/color"
+import { ColorPicker } from '@/components/ui';
+
 import { headwearParts } from '@/components/character-editor/headwear-parts';
 import { headwearThumbnails } from '@/components/character-editor/headwear-thumbnails';
 import { headParts } from '@/components/character-editor/head-parts';
@@ -10,13 +13,11 @@ import { patternParts } from '@/components/character-editor/patterns';
 import { patternThumbnails } from '@/components/character-editor/pattern-thumbnails';
 import torso from '@/assets/character-editor/body/torso.png';
 import hands from '@/assets/character-editor/body/hands.png';
-import armClothe from '@/assets/character-editor/body/arm-clothe.svg';
-import chestClothe from '@/assets/character-editor/body/chest-clothe.svg';
 
 export default function Page() {
     const [selectedCategory, setSelectedCategory] = useState<'head' | 'headwear' | 'clothe'>('head');
     const [semantic, setSemantic] = useState<'white' | 'black'>('white');
-    const [clotheColor, setClotheColor] = useState();
+    const [color, setColor] = useState(parseColor("#002966"));
     const characterParts = {
         // add thumbnails for the parts
         head: headParts.map((part, i) => ({ ...part, thumbnail: headThumbnails[i].src })),
@@ -40,9 +41,24 @@ export default function Page() {
     ]
 
     return (
-        <div className='flex flex-col bg-amber-200 items-center justify-center h-screen space-y-4'>
+        <div className='flex flex-col px-2 bg-amber-200 items-center justify-center h-svh space-y-4'>
+            {/* Color selector */}
+            <div className='w-1/2 relative flex justify-center space-x-4'>
+                <div className='absolute top-4 right-0 flex space-x-4 z-60'>
+                    <ColorPicker
+                        label='Color de ropa'
+                        value={color}
+                        onChange={setColor}
+                    />
+                </div>
+                <div className='absolute top-4 left-0 flex space-x-4 z-60'>
+                    <button className='w-8 h-8 bg-white border-2 border-black rounded-full cursor-pointer' onClick={() => setSemantic('white')} />
+                    <button className='w-8 h-8 bg-black border-2 border-white rounded-full cursor-pointer' onClick={() => setSemantic('black')} />
+                </div>
+            </div>
+
             {/* Character Preview */}
-            <div className='relative w-1/3 h-1/2 bg-amber-50'>
+            <div className='relative inset-x-0 w-1/3 h-1/2 bg-amber-50'>
                 <div className='absolute top-0 z-50'>
                     <Image
                         src={hands}
@@ -67,7 +83,7 @@ export default function Page() {
                         alt='Head preview'
                     />
                 </div>
-                <div className='absolute top-0 z-20'>
+                <div className={`absolute top-0 z-20 ${semantic === 'black' && 'invert'}`}>
                     <Image
                         src={selectedParts.clothe}
                         width={500}
@@ -77,16 +93,16 @@ export default function Page() {
                 </div>
                 <div className='absolute top-0 z-10 mix-blend-multiply w-full max-w-[500px] aspect-square'>
                     <svg className='w-full h-full' viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M352.787 657.849H295.041H294.235L280 605.206L291.818 567.066L362.725 433.041H398.984L352.787 657.849Z" fill="#42CDFF"/>
-                        <path d="M352.787 657.849H295.041H294.235L280 605.206L291.818 567.066L362.725 433.041H398.984L352.787 657.849Z" stroke="#42CDFF"/>
-                        <path d="M673.213 657.581H730.959H731.765L746 604.937L734.182 566.798L663.275 432.772H627.016L673.213 657.581Z" fill="#42CDFF"/>
-                        <path d="M673.213 657.581H730.959H731.765L746 604.937L734.182 566.798L663.275 432.772H627.016L673.213 657.581Z" stroke="#42CDFF"/>
+                        <path d="M352.787 657.849H295.041H294.235L280 605.206L291.818 567.066L362.725 433.041H398.984L352.787 657.849Z" fill={color.toString()}/>
+                        <path d="M352.787 657.849H295.041H294.235L280 605.206L291.818 567.066L362.725 433.041H398.984L352.787 657.849Z" stroke={color.toString()}/>
+                        <path d="M673.213 657.581H730.959H731.765L746 604.937L734.182 566.798L663.275 432.772H627.016L673.213 657.581Z" fill={color.toString()}/>
+                        <path d="M673.213 657.581H730.959H731.765L746 604.937L734.182 566.798L663.275 432.772H627.016L673.213 657.581Z" stroke={color.toString()}/>
                     </svg>
                 </div>
                 <div className='absolute top-0 z-10 w-full max-w-[500px] aspect-square'>
                     <svg className='w-full h-full' viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M518.371 682.29H349.698L400.112 433.441C402.193 420.76 408.385 412.489 423.694 410.479C439.004 408.468 518.371 410.479 518.371 410.479V682.29Z" fill="#42CDFF" stroke="#42CDFF"/>
-                        <path d="M507.762 682.377H676.435L626.021 433.529C623.941 420.848 617.749 412.577 602.439 410.566C587.13 408.556 507.762 410.566 507.762 410.566V682.377Z" fill="#42CDFF" stroke="#42CDFF"/>
+                        <path d="M518.371 682.29H349.698L400.112 433.441C402.193 420.76 408.385 412.489 423.694 410.479C439.004 408.468 518.371 410.479 518.371 410.479V682.29Z" fill={color.toString()} stroke={color.toString()}/>
+                        <path d="M507.762 682.377H676.435L626.021 433.529C623.941 420.848 617.749 412.577 602.439 410.566C587.13 408.556 507.762 410.566 507.762 410.566V682.377Z" fill={color.toString()} stroke={color.toString()}/>
                     </svg>
                 </div>
                 <div className='absolute top-0 z-0'>
@@ -116,12 +132,12 @@ export default function Page() {
             </div>
 
             {/* Options Grid */}
-            <div className='w-1/2 h-1/3 grid grid-cols-5 place-items-center overflow-auto scrollbar p-4 gap-y-5 bg-white border-2 z-60 rounded-2xl'>
+            <div className='w-1/2 h-1/3 grid grid-cols-5 place-items-center overflow-auto scrollbar p-4 gap-y-5 bg-white border-2 border-blacksac z-60 rounded-4xl'>
                 {characterParts[selectedCategory].map(part => (
                     <div
                         key={part.id}
-                        className={`flex-shrink-0 w-24 h-24 p-3 border-2 rounded-lg grid grid-template-stack justify-center items-center cursor-pointer transition-all duration-300 ${
-                            selectedParts[selectedCategory] === part.id
+                        className={`w-24 h-24 p-3 border-2 rounded-lg grid grid-template-stack items-start cursor-pointer transition-all duration-300 ${
+                            selectedParts[selectedCategory] === part.src
                                 ? 'border-blue-500 bg-blue-50'
                                 : 'border-gray-200 hover:border-blue-300 hover:scale-105'
                         }`}
@@ -137,16 +153,19 @@ export default function Page() {
                                 alt={`${selectedCategory} thumbnail`}
                             />
                         )}
-                        <div className='bottom-1 right-1 stroke-green-500 grid-area-stack'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                            </svg>
-                        </div>
+                        {/* Checkmark */}
+                        {selectedParts[selectedCategory] === part.src && (
+                            <div className='h-full flex items-end justify-end fill-green-500 grid-area-stack'>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='size-6'>
+                                    <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        )}
                     </div>
                 ))
                 }
             </div>
-            <div className='w-full flex justify-end'>
+            <div className='w-1/2 mb-2 flex justify-end'>
                 <button className='bg-blacksac hover:bg-neutral-700 text-white font-bold py-2 px-4 rounded'>
                     ¡Terminé!
                 </button>
---
+++ src/components/ui/color-picker.tsx
@@ -0,0 +1,116 @@
+"use client"
+
+import { use } from "react"
+
+import {
+  ColorPicker as ColorPickerPrimitive,
+  type ColorPickerProps as ColorPickerPrimitiveProps,
+  ColorPickerStateContext,
+} from "react-aria-components"
+
+import { cn } from "@/utils/classes"
+import { parseColor } from "@react-stately/color"
+import { IconEyeDropper } from "justd-icons"
+import { twJoin } from "tailwind-merge"
+import { Button } from "./button"
+import { ColorArea } from "./color-area"
+import { ColorField } from "./color-field"
+import { ColorSlider } from "./color-slider"
+import { ColorSwatch } from "./color-swatch"
+import { Description } from "./field"
+import { Popover, PopoverContent, type PopoverContentProps } from "./popover"
+
+interface ColorPickerProps
+  extends ColorPickerPrimitiveProps,
+    Pick<PopoverContentProps, "placement"> {
+  label?: string
+  className?: string
+  children?: React.ReactNode
+  showArrow?: boolean
+  isDisabled?: boolean
+  description?: string
+  eyeDropper?: boolean
+}
+
+const ColorPicker = ({
+  showArrow = false,
+  placement = "bottom start",
+  label,
+  isDisabled,
+  children,
+  description,
+  eyeDropper,
+  className,
+  ...props
+}: ColorPickerProps) => {
+  return (
+    <div className={cn("flex flex-col items-start gap-y-1.5", className)}>
+      <ColorPickerPrimitive {...props}>
+        <Popover>
+          <Button
+            isDisabled={isDisabled}
+            size={label ? "medium" : "square-petite"}
+            intent="secondary"
+            className={twJoin(
+              "*:data-[slot=color-swatch]:-mx-0.5 w-auto px-2.5",
+              !label && "size-10",
+            )}
+          >
+            <ColorSwatch className="size-6" />
+            {label && label}
+          </Button>
+          <PopoverContent
+            className="overflow-auto **:data-[slot=color-area]:w-full **:data-[slot=color-slider]:w-full sm:min-w-min sm:max-w-56 sm:**:data-[slot=color-area]:size-56 *:[[role=dialog]]:p-4 sm:*:[[role=dialog]]:p-3"
+            showArrow={showArrow}
+            placement={placement}
+          >
+            <div className="flex flex-col gap-y-1.5">
+              {children || (
+                <>
+                  <ColorArea colorSpace="hsb" xChannel="saturation" yChannel="brightness" />
+                  <ColorSlider showOutput={false} colorSpace="hsb" channel="hue" />
+                  <div className="flex items-center gap-1.5">
+                    {eyeDropper && <EyeDropper />}
+                    <ColorField className="h-9" aria-label="Hex" />
+                  </div>
+                </>
+              )}
+            </div>
+          </PopoverContent>
+        </Popover>
+      </ColorPickerPrimitive>
+      {description && <Description>{description}</Description>}
+    </div>
+  )
+}
+
+declare global {
+  interface Window {
+    EyeDropper?: new () => { open: () => Promise<{ sRGBHex: string }> }
+  }
+}
+
+const EyeDropper = () => {
+  const state = use(ColorPickerStateContext)!
+
+  if (!window.EyeDropper) {
+    return "EyeDropper is not supported in your browser."
+  }
+
+  return (
+    <Button
+      aria-label="Eye dropper"
+      size="square-petite"
+      intent="outline"
+      onPress={() => {
+        const eyeDropper = window.EyeDropper ? new window.EyeDropper() : null
+        eyeDropper?.open().then((result) => state.setColor(parseColor(result.sRGBHex)))
+      }}
+    >
+      <IconEyeDropper />
+    </Button>
+  )
+}
+
+export type { ColorPickerProps }
+export { ColorPicker, EyeDropper }
