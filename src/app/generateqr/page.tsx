86fe51f37c0914ed56a28c4f4c5a88518b47a2ba
Juan Antonio HR <92959694+JuanAntonioHR@users.noreply.github.com>
2025-03-10 23:42:31 -0600

    fix: vercel compilation

---
+++ src/app/generateqr/page.tsx
@@ -0,0 +1,7 @@
+export const Page = () => {
+    return (
+        <div>
+            <h1>Generate QR</h1>
+        </div>
+    );
+}
---
+++ src/app/viewqr/page.tsx
@@ -0,0 +1,7 @@
+export const Page = () => {
+    return (
+        <div>
+            <h1>View QR</h1>
+        </div>
+    );
+}
