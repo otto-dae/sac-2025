9efbf69ac161a83b0c33eaff261cd659a6c58637
otto-dae <otto.acha.28@gmail.com>
2025-03-12 10:21:02 -0600

    Tryed out solutions, couldnt make them work

--- src/components/landingpage/herosection/BigAhhChicken.tsx
+++ src/components/landingpage/herosection/BigAhhChicken.tsx
@@ -3,26 +3,26 @@ import React from "react";
 export default function BigAhhChicken(){
     return(
         <div id="hero-section-container" className="relative h-screen w-screen">
-            
 
-            <div id="model-container" className="absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2">
-            <div className="w-[429.58px] h-[429.58px] m-auto left-0 right-0 top-[56.42px] bg-[#ffdf61] rounded-full"></div>
-            <img className="w-[800.58px] h-[486px] rounded-bl-[999px] top-0 absolute rounded-br-[999px] " src="BigAhhhhChicken.png" />
-            </div>
-
-            <div id="model-container" className="w-full h-screen relative top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 ">
-            <div className=" w-[251px] h-[251px] bg-[#f75e63] m-auto right-6/28 top-0 ml-100px rounded-full absolute"></div>
-            <div className="w-[122px] h-[122px] bg-[#c5f06f] absolute m-auto right-6/24 top-14/36 rounded-full -translate-x-6/24 -translate-y-4/16"></div>
-            <div className="w-[88px] h-[88px] bg-[#42cdff] m-auto left-12/32 bottom-1/5 rounded-full absolute"></div>
-            </div>
-
-            
+        <div id="model-container" className="absolute top-4/12 left-1/2  -translate-x-1/2 -translate-y-4/12">
+        <div className="w-[429.58px] h-[429.58px] m-auto left-0 right-0 top-[56.42px] bg-[#ffdf61] rounded-full"></div>
+        <img className="w-[800.58px] h-[486px] rounded-bl-[999px] top-0 absolute rounded-br-[999px] " src="BigAhhhhChicken.png" />
         </div>
+
+        <div id="model-container" className="w-full h-screen relative top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 ">
+        <div className=" w-[251px] h-[251px] bg-[#f75e63] m-auto right-8/28 top-0 ml-100px rounded-full absolute shrink"></div>
+        <div className="w-[122px] h-[122px] bg-[#c5f06f] absolute m-auto right-6/24 top-10/36 rounded-full shrink"></div>
+        <div className="w-[88px] h-[88px] bg-[#42cdff] m-auto left-11/32 bottom-6/16 rounded-full absolute shrink"></div>
+        </div>
+
+        <div className="bottom-3/16 left-1/2  -translate-x-1/2 absolute text-center justify-start text-[#1c1c1c] text-[73.05px] font-light font-['Cera_Pro'] leading-[73.05px]">SEMANA ACADÉMICA <br/>CULTURAL 2024</div>
+
+    </div>
     )
 
     /*
-            <div className=" w-[251px] h-[251px] bg-[#f75e63] m-auto right-0 ml-100px rounded-full absolute"></div>
-            <div className="w-[122px] h-[122px] bg-[#c5f06f] static m-auto right-4/16 top-6/24 rounded-full -translate-x-6/24 -translate-y-4/16"></div>
-            <div className="w-[88px] h-[88px] bg-[#42cdff] m-auto left-0 rounded-full absolute"></div>
+            <div className=" w-[251px] h-[251px] bg-[#f75e63] m-auto right-8/28 top-0 ml-100px rounded-full absolute"></div>
+            <div className="w-[122px] h-[122px] bg-[#c5f06f] absolute m-auto right-6/24 top-14/36 rounded-full -translate-x-6/24 -translate-y-4/16"></div>
+            <div className="w-[88px] h-[88px] bg-[#42cdff] m-auto left-12/32 bottom-1/5 rounded-full absolute"></div>
     */
 }
