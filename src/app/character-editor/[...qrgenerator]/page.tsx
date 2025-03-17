d9bfeb49b3a2b16e73d8c96f2659a448928b7dd9
Juan Antonio HR <92959694+JuanAntonioHR@users.noreply.github.com>
2025-03-17 03:47:18 -0600

    feat: add canvas utility functions for image loading and SVG generation

--- src/app/character-editor/[...qrgenerator]/page.tsx
+++ src/app/character-editor/[...qrgenerator]/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { StaticCanvas, FabricImage, FabricText, filters, loadSVGFromString, util, ImageProps, ObjectEvents, SerializedImageProps } from 'fabric';
+import { StaticCanvas, FabricText } from 'fabric';
 
 import qrbg from '@/assets/QRCard.png';
 import qricon from '@/assets/qr-block-icon.png';
@@ -12,7 +12,8 @@ import { patternParts } from '@/components/character-editor/patterns';
 import torso from '@/assets/character-editor/body/torso.png';
 import hands from '@/assets/character-editor/body/hands.png';
 
-import generateQRCode from '@/components/character-editor/CreateQR';
+import { loadImage, applyInvertFilter, createSVG, createQRSVG, SVG_SLEEVE, SVG_TORSO } from '@/utils/canvasUtils';
+
 
 interface Params {
     qrgenerator: string[];
@@ -41,87 +42,6 @@ export default function Page({ params }: { params: Params }) {
             height: 942,
         });
 
-        setCanvasInstance(canvas);
-
-        // Función para cargar imágenes correctamente
-        const loadImage = (src: string, options = {}) => {
-            return new Promise<FabricImage>((resolve, reject) => {
-                const imgElement = new window.Image();
-                imgElement.src = src;
-                imgElement.onload = () => {
-                    const fabricImg = new FabricImage(imgElement, options);
-                    resolve(fabricImg);
-                };
-                imgElement.onerror = () => reject(`Error al cargar la imagen: ${src}`);
-            });
-        };
-
-        const applyInvertFilter = (image: FabricImage<Partial<ImageProps>, SerializedImageProps, ObjectEvents>, shouldInvert: boolean) => {
-            if (shouldInvert) {
-                const invertFilter = new filters.Invert();
-                image.filters.push(invertFilter);
-                image.applyFilters(); // Aplica el filtro
-            }
-        };
-
-        const createSVG1 = () => {
-            const svgMarkup = `
-                <svg viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M352.787 657.849H295.041H294.235L280 605.206L291.818 567.066L362.725 433.041H398.984L352.787 657.849Z" fill="#${playerColor}"/>
-                    <path d="M352.787 657.849H295.041H294.235L280 605.206L291.818 567.066L362.725 433.041H398.984L352.787 657.849Z" stroke="#${playerColor}"/>
-                    <path d="M673.213 657.581H730.959H731.765L746 604.937L734.182 566.798L663.275 432.772H627.016L673.213 657.581Z" fill="#${playerColor}"/>
-                    <path d="M673.213 657.581H730.959H731.765L746 604.937L734.182 566.798L663.275 432.772H627.016L673.213 657.581Z" stroke="#${playerColor}"/>
-                </svg>
-            `;
-            return loadSVGFromString(svgMarkup)
-                .then((parsedSVG) => {
-                    // Obtener los objetos de la propiedad "objects"
-                    const objects = parsedSVG.objects;
-                    // Agrupar los elementos SVG
-                    const svgObject = util.groupSVGElements(objects.filter(obj => obj !== null), {});
-                    svgObject.globalCompositeOperation = 'multiply'
-                    return svgObject;
-                })
-                .catch((error) => {
-                    console.error('Error al crear el SVG:', error);
-                });
-        };
-
-        const createSVG2 = () => {
-            const svgMarkup = `
-                <svg viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M518.371 682.29H349.698L400.112 433.441C402.193 420.76 408.385 412.489 423.694 410.479C439.004 408.468 518.371 410.479 518.371 410.479V682.29Z" fill="#${playerColor}" stroke="#${playerColor}"/>
-                    <path d="M507.762 682.377H676.435L626.021 433.529C623.941 420.848 617.749 412.577 602.439 410.566C587.13 408.556 507.762 410.566 507.762 410.566V682.377Z" fill="#${playerColor}" stroke="#${playerColor}"/>
-                </svg>
-            `;
-            return loadSVGFromString(svgMarkup)
-                .then((parsedSVG) => {
-                    // Obtener los objetos de la propiedad "objects"
-                    const objects = parsedSVG.objects;
-                    // Agrupar los elementos SVG
-                    const svgObject = util.groupSVGElements(objects.filter(obj => obj !== null), {});
-                    return svgObject;
-                })
-                .catch((error) => {
-                    console.error('Error al crear el SVG:', error);
-                });
-        };
-
-        const createQRSVG = () => {
-            const svgMarkup = generateQRCode(userId);
-            return loadSVGFromString(svgMarkup)
-                .then((parsedSVG) => {
-                    // Obtener los objetos de la propiedad "objects"
-                    const objects = parsedSVG.objects;
-                    // Agrupar los elementos SVG
-                    const svgObject = util.groupSVGElements(objects.filter(obj => obj !== null), {});
-                    return svgObject;
-                })
-                .catch((error) => {
-                    console.error('Error al crear el SVG:', error);
-                });
-        };
-
         // Cargar y agregar las imágenes al canvas
         const loadAndDrawImages = async () => {
             try {
@@ -132,34 +52,35 @@ export default function Page({ params }: { params: Params }) {
                 });
                 canvas.backgroundImage = backgroundImage;
 
-                // body
-                const bodyImage1 = await loadImage(torso.src, { left: 52, top: 5, scaleX: 0.5, scaleY: 0.5 });
-                canvas.add(bodyImage1);
-                const svg1 = await createSVG1();
+                // Figura 1
+                await loadAndAddImage(torso.src, { left: 52, top: 5, scaleX: 0.5, scaleY: 0.5 });
+                const svg1 = await createSVG(SVG_SLEEVE.replace(/COLOR/g, playerColor), true);
                 if (svg1) {
                     svg1.set({ left: 192, top: 221, scaleX: 0.5, scaleY: 0.5 });
                     canvas.add(svg1);
                 }
-                const svg2 = await createSVG2();
+                const svg2 = await createSVG(SVG_TORSO.replace(/COLOR/g, playerColor));
                 if (svg2) {
                     svg2.set({ left: 227, top: 209, scaleX: 0.5, scaleY: 0.5 });
                     canvas.add(svg2);
                 }
-                const handsImage1 = await loadImage(hands.src, { left: 52, top: 5, scaleX: 0.5, scaleY: 0.5 });
-                canvas.add(handsImage1);
-                const svg3 = await createSVG1();
+                await loadAndAddImage(hands.src, { left: 52, top: 5, scaleX: 0.5, scaleY: 0.5 });
+
+                // Figura 2
+                await loadAndAddImage(torso.src, { left: 52, top: 444, scaleX: 0.5, scaleY: 0.5, flipY: true });
+                const svg3 = await createSVG(SVG_SLEEVE.replace(/COLOR/g, playerColor), true);
                 if (svg3) {
                     svg3.set({ left: 192, top: 627, scaleX: 0.5, scaleY: 0.5, flipY: true });
                     canvas.add(svg3);
                 }
-                const svg4 = await createSVG2();
+                const svg4 = await createSVG(SVG_TORSO.replace(/COLOR/g, playerColor));
                 if (svg4) {
                     svg4.set({ left: 227, top: 615, scaleX: 0.5, scaleY: 0.5, flipY: true });
                     canvas.add(svg4);
                 }
-                const handsImage2 = await loadImage(hands.src, { left: 52, top: 444, scaleX: 0.5, scaleY: 0.5, flipY: true });
-                canvas.add(handsImage2);
+                loadAndAddImage(hands.src, { left: 52, top: 444, scaleX: 0.5, scaleY: 0.5, flipY: true });
                 
+                // Partes seleccionadas en figura 1
                 for (let i = 0; i < selectedImages.length; i++) {
                     const part = selectedImages[i];
                     if (!part) continue;
@@ -168,6 +89,7 @@ export default function Page({ params }: { params: Params }) {
                     canvas.add(img);
                 }
                 
+                // Partes seleccionadas en figura 2
                 for (let i = 0; i < selectedImages.length; i++) {
                     const part = selectedImages[i];
                     if (!part) continue;
@@ -176,6 +98,7 @@ export default function Page({ params }: { params: Params }) {
                     canvas.add(img);
                 }
 
+                // Textos
                 const text1 = new FabricText(userId, {
                     left: 50,
                     top: 555,
@@ -197,10 +120,9 @@ export default function Page({ params }: { params: Params }) {
                 });
                 canvas.add(text2);
 
-                const qrIconImage = await loadImage(qricon.src, { left: -5, top: 0 });
-                canvas.add(qrIconImage);
-
-                const qrSVG = await createQRSVG();
+                // QR
+                await loadAndAddImage(qricon.src, { left: -5, top: 0 });
+                const qrSVG = await createQRSVG(userId);
                 if (qrSVG) {
                     qrSVG.set({ left: 210, top: 381, scaleX: 9, scaleY: 9 });
                     canvas.add(qrSVG);
@@ -211,6 +133,11 @@ export default function Page({ params }: { params: Params }) {
             } catch (error) {
                 console.error(error);
             }
+        };
+
+        const loadAndAddImage = async (src: string, options: {} | undefined) => {
+            const img = await loadImage(src, options);
+            canvas.add(img);
         };
 
         loadAndDrawImages();
