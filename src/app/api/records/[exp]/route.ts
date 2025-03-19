a7ef53c0a942e370725e21f2adbbf83481f25212
Karlo Paz <karlopazpory@gmail.com>
2025-03-19 12:39:54 -0600

    feat: Better api response

--- src/app/api/records/[exp]/route.ts
+++ src/app/api/records/[exp]/route.ts
@@ -8,11 +8,16 @@ export async function GET(
   { params }: { params: Promise<{ exp: string }> },
 ) {
   const {exp} = await params;
-  const student = await db
+  const registrarOfStudent = await db
     .select()
     .from(studentsToWorkshops)
     .innerJoin(students, eq(students.expediente, studentsToWorkshops.studentId))
     .innerJoin(workshops, eq(workshops.id, studentsToWorkshops.workshopId))
     .where(eq(studentsToWorkshops.studentId, parseInt(exp)))
-  return NextResponse.json(student);
+  const response = {
+    student: registrarOfStudent[0].students.nombre,
+    exp: registrarOfStudent[0].students.expediente,
+    workshops: registrarOfStudent.map((s) => s.workshops),
+  }
+  return NextResponse.json(response);
 }
