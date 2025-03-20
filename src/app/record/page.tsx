9087d107b06c6215f61741b7444fcffbaa10c8fa
Jesus Guzman <amorales130@alumnos.uaq.mx>
2025-03-20 00:18:43 -0600

    Fix: Bugs and delete empty pages

--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import Image from 'next/image';
 
 export default function Record() {
-    return (
-        <div className='bg-amber-400'>Record</div>
-    );
+  return (
+    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+      <Image
+        src="/Asistencia.svg" 
+        alt="Asistencia"
+        fill 
+        style={{ objectFit: 'cover' }} 
+      />
+    </div>
+  );
 }
--- src/app/viewqr/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Page() {
-    return (
-        <div>
-            <h1>View QR</h1>
-        </div>
-    );
-}
