41bc8e48b64b30e8536a6a794680b8a19a1e8ed8
BryanBonilla <bbonilla04@alumnos.uaq.mx>
2025-03-21 20:47:31 -0600

    fix: record page

--- src/components/dwlrecord/LegoModal.tsx
+++ src/components/dwlrecord/LegoModal.tsx
@@ -22,21 +22,22 @@ const LegoModal = () => {
   };
 
   return (
-    <div className="flex flex-col justify-end items-center z-50 h-screen relative">
-      <div className="flex flex-col justify-end items-center w-full h-full mb-70"> 
+    <div className="flex flex-col justify-start items-center z-50 h-screen relative">
+      <div className="flex flex-col justify-start items-center w-full h-full mt-20"> 
         <input
           type="number"
           value={expediente}
           onChange={(e) => setExpediente(e.target.value)}
           onKeyDown={(e) => e.key === "Enter" && openModal()}
           placeholder="Ingresa tu expediente..."
-          className="border-1 border-gray-700 rounded-3xl p-1 md:p-2 w-full max-w-md focus:outline-none placeholder:text-xl placeholder:p-3"
+          className="border-1 border-gray-700 rounded-3xl p-1 md:p-2 w-[90%] max-w-[500px] focus:outline-none placeholder:text-xl placeholder:p-3"
         />
       </div>
 
       {isOpen && (
         <div className="fixed inset-0 backdrop-blur-xs bg-opacity-50 flex justify-center items-center z-50 overflow-hidden">
-          <div className="rounded-3xl relative min-h-[600px] min-w-[300px] bg-gradient-to-r from-[#f87171] from-50% to-white to-50% overflow-hidden sm:min-h-[300px] sm:min-w-[500px] md:min-h-[400px] md:min-w-[700px]">
+          {/* Modal con dimensiones fijas y propio tamaño */}
+          <div className="rounded-3xl relative min-h-[400px] min-w-[500px] bg-gradient-to-r from-[#f87171] from-50% to-white to-50% overflow-hidden sm:min-h-[300px] sm:min-w-[500px] md:min-h-[400px] md:min-w-[700px]">
             <div className="w-[140px] h-[140px] absolute top-24 left-0 z-20 ml-[2px] sm:top-0 sm:p-2 md:p-0 md:w-fit md:h-fit md:mt-[30px] md:ml-[10px]">
               <TextBubbledwlr />
             </div>
