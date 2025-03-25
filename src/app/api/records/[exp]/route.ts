ae3ea1f8e836df0b1bd5355a601063cd845e2742
Karlo Paz <karlopazpory@gmail.com>
2025-03-25 15:14:25 -0600

    fix: Added 404 response in api route

--- src/app/api/records/[exp]/route.ts
+++ src/app/api/records/[exp]/route.ts
@@ -14,6 +14,9 @@ export async function GET(
     .innerJoin(students, eq(students.expediente, studentsToWorkshops.studentId))
     .innerJoin(workshops, eq(workshops.id, studentsToWorkshops.workshopId))
     .where(eq(studentsToWorkshops.studentId, parseInt(exp)))
+  if (!registrarOfStudent[0]?.students) {
+    return NextResponse.json({ error: "Student not found" }, { status: 404 });
+  }
   const response = {
     student: registrarOfStudent[0].students.nombre,
     exp: registrarOfStudent[0].students.expediente,
