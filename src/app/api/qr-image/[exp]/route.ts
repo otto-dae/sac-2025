5b65681092a04c8ab0121d44e6b1c2dc5b772046
Alejandro Macias <alex.mac.fon@gmail.com>
2025-03-25 10:13:39 -0600

    style: indent

--- src/app/api/qr-image/[exp]/route.ts
+++ src/app/api/qr-image/[exp]/route.ts
@@ -7,21 +7,20 @@ export async function GET(
   { params }: { params: Promise<{ exp: string }> },
 ) {
   try {
-  const {exp} = await params;
-  const studentQr = await db
-  .select({studentNumber: students.expediente,
-  lego_image: students.lego_image})
-  .from(students)
-  .where(eq(students.expediente, parseInt(exp)));
-  if(studentQr.length === 0){
-    return NextResponse.json({error: 'Student not found'}, {status: 404});
-  }
-  const { studentNumber, lego_image } = studentQr[0];
-  const response = {
-    expediente: studentNumber,
-    lego_image: lego_image
-  }
-  return NextResponse.json(response, {status: 200});
+    const {exp} = await params;
+    const studentQr = await db
+    .select({studentNumber: students.expediente, lego_image: students.lego_image})
+    .from(students)
+    .where(eq(students.expediente, parseInt(exp)));
+    if(studentQr.length === 0){
+      return NextResponse.json({error: 'Student not found'}, {status: 404});
+    }
+    const { studentNumber, lego_image } = studentQr[0];
+    const response = {
+      expediente: studentNumber,
+      lego_image: lego_image
+    }
+    return NextResponse.json(response, {status: 200});
   } catch(error){
     return NextResponse.json(
       { error: "Bad Request" },
